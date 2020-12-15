fd08236453c960646500710ee7a142bcc1257f9b
megfly <mflynn333@gmail.com>
2020-12-15 16:33:55 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,69 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
+
+  //This component takes one prop: 
+  //handleLogin which is a function — this function is called when the form is being submitted
+//   <input
+//   id="username"
+//   type="text"
+//   name="username"
+//   value={this.state.username}
+//   onChange={this.handleInputChange}
+// />
+
+handleInputChange = (event) => {
+  //console.log(event.target.value)
+  this.setState({
+    username: event.target.value, 
+  })
+}
+
+handlePasswordChange = (event) => {
+  //console.log(event.target.value)
+  this.setState({
+    password: event.target.value, 
+  })
+}
+
+handleSubmit = event => {
+  event.preventDefault()
+  if ((this.state.username !== "") && (this.state.password !=="")) {
+    //debugger
+    this.props.handleLogin(this.state)
+  }
+}
+
+// handleSubmit = event => {
+//   event.preventDefault()
+//   this.sendFormDataSomewhere(this.state)
+// }
 
   render() {
     return (
-      <form>
+      // callback prop should call the `handleLogin` callback prop when the form is being submitted
+      <form onSubmit={this.handleSubmit}> 
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text"
+                 value={this.state.username}
+                 onChange={this.handleInputChange}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" 
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
+            />
           </label>
         </div>
         <div>
